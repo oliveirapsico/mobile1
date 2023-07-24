0fd08f4f23de6baeed487e7703c93828984390cc
Camisa 19 <133806325+oliveirapsico@users.noreply.github.com>
2023-07-24 17:59:31 -0300

    Add files via upload

---
+++ CALC_MEDIAJS/index3.js
@@ -0,0 +1,12 @@
+function clicou() {
+    var media=document.getElementById('media');
+    var br1=document.getElementById('barra1');
+    var br2=document.getElementById('barra2');
+
+    var br1= Number(b1.value);
+    var br2= Number(b2.value);
+    var media=string(media.string)
+    var valor= [600-(b1*2+b2*3)]/5;
+    document.getElementById('media').innerHTML=valor;
+    document.getElementById('nota-necessaria').innerHTML='nota necessaria'
+}
